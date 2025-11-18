e7464b66d2f3850fa98dccdfe6da5d9655888fbd
Çağan Mert İŞLEK <hello@islekcaganmert.me>
2025-11-18 13:10:15 +0300

    Bugfix on pkg/httpserver/activitypubhandler/accounts.go -> HandleAccounts

--- pkg/httpserver/activitypubhandler/accounts.go
+++ pkg/httpserver/activitypubhandler/accounts.go
@@ -13,10 +13,13 @@ func HandleAccounts(kernel interfaces.Kernel, w http.ResponseWriter, r *http.Req
 	acceptMimetype := r.Header.Get("Accept")
 	acceptMimetype = strings.SplitN(acceptMimetype, ";", 2)[0]
 	acceptMimetypes := strings.Split(acceptMimetype, ",")
-	if acceptMimetypes[0] != "application/activity+json" && acceptMimetypes[0] != "application/ld+json" &&
-		acceptMimetypes[1] != "application/activity+json" && acceptMimetypes[1] != "application/ld+json" {
-		http.Redirect(w, r, fmt.Sprintf("/~%s", username), http.StatusSeeOther)
-		return
+	if len(acceptMimetypes) != 0 &&
+		acceptMimetypes[0] != "application/activity+json" && acceptMimetypes[0] != "application/ld+json" {
+		if len(acceptMimetypes) == 2 &&
+			acceptMimetypes[1] != "application/activity+json" && acceptMimetypes[1] != "application/ld+json" {
+			http.Redirect(w, r, fmt.Sprintf("/~%s", username), http.StatusSeeOther)
+			return
+		}
 	}
 	user, err := kernel.UserManager().Get(username)
 	if err != nil {
